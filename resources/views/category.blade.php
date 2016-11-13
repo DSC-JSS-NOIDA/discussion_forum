05f05d4d63612bfd616a0799a7dcf48915a06e59
Shashank Agarwal <shashaa35@gmAil.com>
2016-11-14 01:21:57 +0530

    trying to fix bugs

--- resources/views/category.blade.php
+++
@@ -1,7 +0,0 @@
-@foreach($categories as $category)
-	<a href="/category/{{$category->category_id}}" ref="">
-		{{ $category->category_name }}
-		{{ $category->article_count }}
-	</a>
-	<br>
-@endforeach
